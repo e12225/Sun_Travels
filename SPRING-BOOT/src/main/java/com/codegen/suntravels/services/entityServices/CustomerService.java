25ea9d1797510c427b1a0016282f0d50df84f0ab
Subhani <subhani@codegen.net>
2017-12-11 19:37:32 +0530

    submitting version 1

--- SPRING-BOOT/src/main/java/com/codegen/suntravels/services/entityServices/CustomerService.java
+++ SPRING-BOOT/src/main/java/com/codegen/suntravels/services/entityServices/CustomerService.java
@@ -16,7 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CustomerService {
+public class CustomerService
+{
 
     @Autowired
     private CustomerDAO customerDAO;
@@ -27,111 +28,123 @@ public class CustomerService {
     @Autowired
     private CityDAO cityDAO;
 
-    public List<CustomerListResponse> getCustomerList() {
+    public List<CustomerListResponse> getCustomerList()
+    {
 
         List<CustomerListResponse> response = new ArrayList<>();
 
         List<Customer> list = customerDAO.getCustomerList();
 
-        for (Customer customer : list) {
+        for( Customer customer : list )
+        {
             CustomerListResponse r = new CustomerListResponse();
 
-            r.setCustomerID(customer.getCustomerID());
-            r.setCustomerFirstName(customer.getCustomerFirstName());
-            r.setCustomerLastName(customer.getCustomerLastName());
-            r.setCustomerPhoneNumber(customer.getCustomerPhoneNumber());
-            r.setCountryName(countryDAO.getCountryByID(customer.getCountryID()).getCountryName());
-            r.setCityName(cityDAO.getCityByID(customer.getCityID()).getCityName());
-            r.setSsnOrNic(customer.getSsnOrNic());
-            response.add(r);
+            r.setCustomerID( customer.getCustomerID() );
+            r.setCustomerFirstName( customer.getCustomerFirstName() );
+            r.setCustomerLastName( customer.getCustomerLastName() );
+            r.setCustomerPhoneNumber( customer.getCustomerPhoneNumber() );
+            r.setCountryName( countryDAO.getCountryByID( customer.getCountryID() ).getCountryName() );
+            r.setCityName( cityDAO.getCityByID( customer.getCityID() ).getCityName() );
+            r.setSsnOrNic( customer.getSsnOrNic() );
+            response.add( r );
         }
 
         return response;
     }
 
-    public List<CustomerListResponse> getCustomerByNameOrAlias(String name) {
+    public List<CustomerListResponse> getCustomerByNameOrAlias( String name )
+    {
 
         List<CustomerListResponse> response = new ArrayList<>();
 
-        List<Customer> list = customerDAO.getCustomerByNameOrAlias(name);
+        List<Customer> list = customerDAO.getCustomerByNameOrAlias( name );
 
-        for (Customer customer : list) {
+        for( Customer customer : list )
+        {
             CustomerListResponse r = new CustomerListResponse();
 
-            r.setCustomerID(customer.getCustomerID());
-            r.setCustomerFirstName(customer.getCustomerFirstName());
-            r.setCustomerLastName(customer.getCustomerLastName());
-            r.setCustomerPhoneNumber(customer.getCustomerPhoneNumber());
-            r.setCountryName(countryDAO.getCountryByID(customer.getCountryID()).getCountryName());
-            r.setCityName(cityDAO.getCityByID(customer.getCityID()).getCityName());
-            r.setSsnOrNic(customer.getSsnOrNic());
-            response.add(r);
+            r.setCustomerID( customer.getCustomerID() );
+            r.setCustomerFirstName( customer.getCustomerFirstName() );
+            r.setCustomerLastName( customer.getCustomerLastName() );
+            r.setCustomerPhoneNumber( customer.getCustomerPhoneNumber() );
+            r.setCountryName( countryDAO.getCountryByID( customer.getCountryID() ).getCountryName() );
+            r.setCityName( cityDAO.getCityByID( customer.getCityID() ).getCityName() );
+            r.setSsnOrNic( customer.getSsnOrNic() );
+            response.add( r );
         }
 
         return response;
     }
 
-    public CustomerListResponse getCustomerByIdentity(String identity){
+    public CustomerListResponse getCustomerByIdentity( String identity )
+    {
 
         CustomerListResponse response = new CustomerListResponse();
 
-        Customer customer = customerDAO.getCustomerByIdentity(identity);
+        Customer customer = customerDAO.getCustomerByIdentity( identity );
 
-        response.setCustomerID(customer.getCustomerID());
-        response.setCustomerFirstName(customer.getCustomerFirstName());
-        response.setCustomerLastName(customer.getCustomerLastName());
-        response.setCustomerPhoneNumber(customer.getCustomerPhoneNumber());
-        response.setCountryName(countryDAO.getCountryByID(customer.getCountryID()).getCountryName());
-        response.setCityName(cityDAO.getCityByID(customer.getCityID()).getCityName());
-        response.setSsnOrNic(customer.getSsnOrNic());
+        response.setCustomerID( customer.getCustomerID() );
+        response.setCustomerFirstName( customer.getCustomerFirstName() );
+        response.setCustomerLastName( customer.getCustomerLastName() );
+        response.setCustomerPhoneNumber( customer.getCustomerPhoneNumber() );
+        response.setCountryName( countryDAO.getCountryByID( customer.getCountryID() ).getCountryName() );
+        response.setCityName( cityDAO.getCityByID( customer.getCityID() ).getCityName() );
+        response.setSsnOrNic( customer.getSsnOrNic() );
 
         return response;
     }
 
-    public AddEntityResponse addCustomer(AddCustomerRequest request) {
+    public AddEntityResponse addCustomer( AddCustomerRequest request )
+    {
 
         AddEntityResponse response = new AddEntityResponse();
 
-        Country co = countryDAO.getCountryByName(request.getCountryName());
+        Country co = countryDAO.getCountryByName( request.getCountryName() );
 
-        if(co != null){
+        if( co != null )
+        {
 
-            City ci = cityDAO.getCityBelongstoGivenCountry(co.getCountryID(), request.getCityName());
+            City ci = cityDAO.getCityBelongstoGivenCountry( co.getCountryID(), request.getCityName() );
 
-            if(ci == null){
-                response.setInsertingStatus(false);
-                response.setEntity(null);
-                response.setMessage("A city named " + request.getCityName() + " under the given country named " + request.getCountryName() + " doesn't exists in the system");
+            if( ci == null )
+            {
+                response.setInsertingStatus( false );
+                response.setEntity( null );
+                response.setMessage( "A city named " + request.getCityName() + " under the given country named " + request.getCountryName() + " doesn't exists in the system" );
             }
-            else {
-                Customer cu = customerDAO.getCustomerByIdentity(request.getSsnOrNic());
+            else
+            {
+                Customer cu = customerDAO.getCustomerByIdentity( request.getSsnOrNic() );
 
-                if(cu != null){
-                    response.setInsertingStatus(false);
-                    response.setEntity(null);
-                    response.setMessage("A customer with SSN or NIC : " + request.getSsnOrNic() + "  already exists in the system");
+                if( cu != null )
+                {
+                    response.setInsertingStatus( false );
+                    response.setEntity( null );
+                    response.setMessage( "A customer with SSN or NIC : " + request.getSsnOrNic() + "  already exists in the system" );
                 }
-                else {
+                else
+                {
                     Customer customer = new Customer();
-                    customer.setCustomerFirstName(request.getCustomerFirstName());
-                    customer.setCustomerLastName(request.getCustomerLastName());
-                    customer.setCustomerPhoneNumber(request.getCustomerPhoneNumber());
-                    customer.setCountryID(co.getCountryID());
-                    customer.setCityID(ci.getCityID());
-                    customer.setSsnOrNic(request.getSsnOrNic());
+                    customer.setCustomerFirstName( request.getCustomerFirstName() );
+                    customer.setCustomerLastName( request.getCustomerLastName() );
+                    customer.setCustomerPhoneNumber( request.getCustomerPhoneNumber() );
+                    customer.setCountryID( co.getCountryID() );
+                    customer.setCityID( ci.getCityID() );
+                    customer.setSsnOrNic( request.getSsnOrNic() );
 
-                    customerDAO.addCustomer(customer);
+                    customerDAO.addCustomer( customer );
 
-                    response.setInsertingStatus(true);
-                    response.setEntity(customer);
-                    response.setMessage("A new customer successfully added to the system");
+                    response.setInsertingStatus( true );
+                    response.setEntity( customer );
+                    response.setMessage( "A new customer successfully added to the system" );
                 }
             }
         }
-        else {
-            response.setInsertingStatus(false);
-            response.setEntity(null);
-            response.setMessage("A country named " + request.getCountryName() + " doesn't exist in the system");
+        else
+        {
+            response.setInsertingStatus( false );
+            response.setEntity( null );
+            response.setMessage( "A country named " + request.getCountryName() + " doesn't exist in the system" );
         }
 
         return response;
